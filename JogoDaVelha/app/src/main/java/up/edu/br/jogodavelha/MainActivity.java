3bb3e0a6b06fc1a0b31a907f1f05f0e61892abdb
Marcelo Cezário <marcelocezario@gmail.com>
2018-05-14 22:21:43 -0300

    Criando validações de jogo ganho

--- JogoDaVelha/app/src/main/java/up/edu/br/jogodavelha/MainActivity.java
+++ JogoDaVelha/app/src/main/java/up/edu/br/jogodavelha/MainActivity.java
@@ -3,7 +3,9 @@ package up.edu.br.jogodavelha;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +21,12 @@ public class MainActivity extends AppCompatActivity {
     ImageView imageView7;
     ImageView imageView8;
     ImageView imageView9;
+
+    TextView txtVitoriasCirculo;
+    TextView txtVitoriasX;
+    TextView txtEmpates;
+
+    int vitoriasCirculo = 0, vitoriasX = 0, empates = 0;
 
 
     public void limparTela (){
@@ -42,133 +50,137 @@ public class MainActivity extends AppCompatActivity {
         imageView9.setTag((R.drawable.vazio));
     }
 
+    public void vitoriaCirculo(){
+        Toast.makeText(getApplicationContext(),
+                "Círculo Ganhou!", Toast.LENGTH_LONG).show();
+        limparTela();
+        vitoriasCirculo ++;
+        txtVitoriasCirculo.setText("Círculo: " + vitoriasCirculo);
+    }
+
+    public void vitoriaX (){
+        Toast.makeText(getApplicationContext(),
+                "X Ganhou!", Toast.LENGTH_LONG).show();
+        limparTela();
+        vitoriasX ++;
+        txtVitoriasX.setText("X: " + vitoriasX);
+    }
+
+    public void empate(){
+        Toast.makeText(getApplicationContext(),
+                "Empate!", Toast.LENGTH_LONG).show();
+        limparTela();
+        empates ++;
+        txtEmpates.setText("Empates: " + empates);
+    }
+
     public void validarJogada() {
 
         //validando linha 1
         if ((Integer) imageView1.getTag() == R.drawable.circulo
                 && (Integer) imageView2.getTag() == R.drawable.circulo
                 && (Integer) imageView3.getTag() == R.drawable.circulo) {
-            Toast.makeText(getApplicationContext(),
-                    "Círculo Ganhou!", Toast.LENGTH_LONG).show();
-            limparTela();
+            vitoriaCirculo();
         } else {
             if ((Integer) imageView1.getTag() == R.drawable.x
                     && (Integer) imageView2.getTag() == R.drawable.x
                     && (Integer) imageView3.getTag() == R.drawable.x) {
-                Toast.makeText(getApplicationContext(),
-                        "X Ganhou!", Toast.LENGTH_LONG).show();
-                limparTela();
+                vitoriaX();
             } else {
 
                 //validando linha 2
                 if ((Integer) imageView4.getTag() == R.drawable.circulo
                         && (Integer) imageView5.getTag() == R.drawable.circulo
                         && (Integer) imageView6.getTag() == R.drawable.circulo) {
-                    Toast.makeText(getApplicationContext(),
-                            "Círculo Ganhou!", Toast.LENGTH_LONG).show();
-                    limparTela();
+                    vitoriaCirculo();
                 } else {
                     if ((Integer) imageView4.getTag() == R.drawable.x
                             && (Integer) imageView5.getTag() == R.drawable.x
                             && (Integer) imageView6.getTag() == R.drawable.x) {
-                        Toast.makeText(getApplicationContext(),
-                                "X Ganhou!", Toast.LENGTH_LONG).show();
-                        limparTela();
+                        vitoriaX();
                     } else {
                         //validando linha 3
                         if ((Integer) imageView7.getTag() == R.drawable.circulo
                                 && (Integer) imageView8.getTag() == R.drawable.circulo
                                 && (Integer) imageView9.getTag() == R.drawable.circulo) {
-                            Toast.makeText(getApplicationContext(),
-                                    "Círculo Ganhou!", Toast.LENGTH_LONG).show();
-                            limparTela();
+                            vitoriaCirculo();
                         } else {
                             if ((Integer) imageView7.getTag() == R.drawable.x
                                     && (Integer) imageView8.getTag() == R.drawable.x
                                     && (Integer) imageView9.getTag() == R.drawable.x) {
-                                Toast.makeText(getApplicationContext(),
-                                        "X Ganhou!", Toast.LENGTH_LONG).show();
-                                limparTela();
+                                vitoriaX();
                             } else {
 
                                 //validando coluna 1
                                 if ((Integer) imageView1.getTag() == R.drawable.circulo
                                         && (Integer) imageView4.getTag() == R.drawable.circulo
                                         && (Integer) imageView7.getTag() == R.drawable.circulo) {
-                                    Toast.makeText(getApplicationContext(),
-                                            "Círculo Ganhou!", Toast.LENGTH_LONG).show();
-                                    limparTela();
+                                    vitoriaCirculo();
                                 } else {
                                     if ((Integer) imageView1.getTag() == R.drawable.x
                                             && (Integer) imageView4.getTag() == R.drawable.x
                                             && (Integer) imageView7.getTag() == R.drawable.x) {
-                                        Toast.makeText(getApplicationContext(),
-                                                "X Ganhou!", Toast.LENGTH_LONG).show();
-                                        limparTela();
+                                        vitoriaX();
                                     } else {
 
                                         //validando coluna 2
                                         if ((Integer) imageView2.getTag() == R.drawable.circulo
                                                 && (Integer) imageView5.getTag() == R.drawable.circulo
                                                 && (Integer) imageView8.getTag() == R.drawable.circulo) {
-                                            Toast.makeText(getApplicationContext(),
-                                                    "Círculo Ganhou!", Toast.LENGTH_LONG).show();
-                                            limparTela();
+                                            vitoriaCirculo();
                                         } else {
                                             if ((Integer) imageView2.getTag() == R.drawable.x
                                                     && (Integer) imageView5.getTag() == R.drawable.x
                                                     && (Integer) imageView8.getTag() == R.drawable.x) {
-                                                Toast.makeText(getApplicationContext(),
-                                                        "X Ganhou!", Toast.LENGTH_LONG).show();
-                                                limparTela();
+                                                vitoriaX();
                                             } else {
                                                 //validando coluna 3
                                                 if ((Integer) imageView3.getTag() == R.drawable.circulo
                                                         && (Integer) imageView6.getTag() == R.drawable.circulo
                                                         && (Integer) imageView9.getTag() == R.drawable.circulo) {
-                                                    Toast.makeText(getApplicationContext(),
-                                                            "Círculo Ganhou!", Toast.LENGTH_LONG).show();
-                                                    limparTela();
+                                                    vitoriaCirculo();
                                                 } else {
                                                     if ((Integer) imageView3.getTag() == R.drawable.x
                                                             && (Integer) imageView6.getTag() == R.drawable.x
                                                             && (Integer) imageView9.getTag() == R.drawable.x) {
-                                                        Toast.makeText(getApplicationContext(),
-                                                                "X Ganhou!", Toast.LENGTH_LONG).show();
-                                                        limparTela();
+                                                        vitoriaX();
                                                     } else {
 
                                                         //validando transversal 1
                                                         if ((Integer) imageView1.getTag() == R.drawable.circulo
                                                                 && (Integer) imageView5.getTag() == R.drawable.circulo
                                                                 && (Integer) imageView9.getTag() == R.drawable.circulo) {
-                                                            Toast.makeText(getApplicationContext(),
-                                                                    "Círculo Ganhou!", Toast.LENGTH_LONG).show();
-                                                            limparTela();
+                                                            vitoriaCirculo();
                                                         } else {
                                                             //validando transversal 1
                                                             if ((Integer) imageView1.getTag() == R.drawable.x
                                                                     && (Integer) imageView5.getTag() == R.drawable.x
                                                                     && (Integer) imageView9.getTag() == R.drawable.x) {
-                                                                Toast.makeText(getApplicationContext(),
-                                                                        "X Ganhou!", Toast.LENGTH_LONG).show();
-                                                                limparTela();
+                                                                vitoriaX();
                                                             } else {
 
                                                                 //validando transversal 2
                                                                 if ((Integer) imageView3.getTag() == R.drawable.circulo
                                                                         && (Integer) imageView5.getTag() == R.drawable.circulo
                                                                         && (Integer) imageView7.getTag() == R.drawable.circulo) {
-                                                                    Toast.makeText(getApplicationContext(),
-                                                                            "Círculo Ganhou!", Toast.LENGTH_LONG).show();
-                                                                    limparTela();
+                                                                    vitoriaCirculo();
                                                                 } else {
                                                                     if ((Integer) imageView3.getTag() == R.drawable.x
                                                                             && (Integer) imageView5.getTag() == R.drawable.x
                                                                             && (Integer) imageView7.getTag() == R.drawable.x) {
-                                                                        Toast.makeText(getApplicationContext(),
-                                                                                "X Ganhou!", Toast.LENGTH_LONG).show();
-                                                                        limparTela();
+                                                                        vitoriaX();
+                                                                    } else {
+                                                                        if ((Integer) imageView1.getTag() != R.drawable.vazio
+                                                                                && (Integer) imageView2.getTag() != R.drawable.vazio
+                                                                                && (Integer) imageView3.getTag() != R.drawable.vazio
+                                                                                && (Integer) imageView4.getTag() != R.drawable.vazio
+                                                                                && (Integer) imageView5.getTag() != R.drawable.vazio
+                                                                                && (Integer) imageView6.getTag() != R.drawable.vazio
+                                                                                && (Integer) imageView7.getTag() != R.drawable.vazio
+                                                                                && (Integer) imageView8.getTag() != R.drawable.vazio
+                                                                                && (Integer) imageView9.getTag() != R.drawable.vazio){
+                                                                            empate();
+                                                                        }
                                                                     }
                                                                 }
                                                             }
@@ -192,6 +204,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtVitoriasCirculo = (TextView)findViewById(R.id.txtVitoriasCirculo);
+        txtVitoriasX = (TextView)findViewById(R.id.txtVitoriasX);
+        txtEmpates = (TextView)findViewById(R.id.txtEmpates);
+
+
 
         imageView1 = (ImageView) findViewById(R.id.imageView1);
         imageView1.setOnClickListener(new View.OnClickListener() {
