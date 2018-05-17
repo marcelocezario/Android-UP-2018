4948e9e393c7fb79f55e3fff5b5cb62b50ac848c
Marcelo Cezário <marcelocezario@gmail.com>
2018-05-16 21:26:49 -0300

    Criando lista de contatos

--- Contatos/app/src/main/java/up/edu/br/contatos/MainActivity.java
+++ Contatos/app/src/main/java/up/edu/br/contatos/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,6 +34,9 @@ public class MainActivity extends AppCompatActivity {
                 //        .setAction("Action", null).show();
             }
         });
+
+        ListView listaContatos = (ListView)findViewById(R.id.listaContatos);
+        
     }
 
     @Override
