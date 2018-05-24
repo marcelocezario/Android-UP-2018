82321d9c404e048507e45d02ee8c780350e2abe1
Marcelo Cezário <marcelocezario@gmail.com>
2018-05-23 22:00:56 -0300

    Criando metodo para notificar no horário selecionado

--- Notificacao/app/src/main/java/up/edu/br/notificacao/MainActivity.java
+++ Notificacao/app/src/main/java/up/edu/br/notificacao/MainActivity.java
@@ -6,8 +6,12 @@ import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import java.sql.Time;
+import java.util.Calendar;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,12 +24,21 @@ public class MainActivity extends AppCompatActivity {
     public void agendar(View view) {
 
         EditText txtDescricao = (EditText) findViewById(R.id.txtDescricao);
-
         DatePicker data = (DatePicker) findViewById(R.id.data);
-
         TimePicker hora = (TimePicker) findViewById(R.id.hora);
 
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.DAY_OF_MONTH,data.getDayOfMonth());
+        calendar.set(Calendar.MONTH, data.getMonth());
+        calendar.set(Calendar.YEAR, data.getYear());
+        calendar.set(Calendar.HOUR_OF_DAY, hora.getCurrentHour());
+        calendar.set(Calendar.MINUTE, hora.getCurrentMinute());
 
-
+        Timer t = new Timer();
+        t.schedule (new TimerTask(){
+            public void run(){
+                Toast.makeText(getApplicationContext(),"Rodou",Toast.LENGTH_LONG).show();
+            }
+        }, calendar.getTime());
     }
 }
