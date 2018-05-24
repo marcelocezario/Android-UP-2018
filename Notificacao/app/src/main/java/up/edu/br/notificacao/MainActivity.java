2c97b7572bf8a64ce81e0fb89d66644d95a29008
Marcelo Cezário <marcelocezario@gmail.com>
2018-05-23 21:35:47 -0300

    Criando comandos botão Agendar

--- Notificacao/app/src/main/java/up/edu/br/notificacao/MainActivity.java
+++ Notificacao/app/src/main/java/up/edu/br/notificacao/MainActivity.java
@@ -2,6 +2,12 @@ package up.edu.br.notificacao;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+
+import java.sql.Time;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void agendar(View view) {
+
+        EditText txtDescricao = (EditText) findViewById(R.id.txtDescricao);
+
+        DatePicker data = (DatePicker) findViewById(R.id.data);
+
+        TimePicker hora = (TimePicker) findViewById(R.id.hora);
+
+
+
     }
 }
