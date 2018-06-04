484fc72965e2bd9d6a650e02954b6625b9fa2f38
Marcelo Cezário <marcelocezario@gmail.com>
2018-06-04 19:37:59 -0300

    Criando ContatoAdapter extends BaseAdapter e implementando

--- Contatos/app/src/main/java/up/edu/br/contatos/ContatoAdapter.java
+++ Contatos/app/src/main/java/up/edu/br/contatos/ContatoAdapter.java
@@ -1,4 +1,27 @@
 package up.edu.br.contatos;
 
-public class ContatoAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class ContatoAdapter extends BaseAdapter{
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
 }
