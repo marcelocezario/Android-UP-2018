8372a07fc6a107ec6a44f6d73dc58aba5adbff70
Marcelo Cezário <marcelocezario@gmail.com>
2018-06-04 19:43:44 -0300

    Criando ContatoAdapter

--- Contatos/app/src/main/java/up/edu/br/contatos/ContatoAdapter.java
+++ Contatos/app/src/main/java/up/edu/br/contatos/ContatoAdapter.java
@@ -1,13 +1,27 @@
 package up.edu.br.contatos;
 
+import android.app.Activity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
+import java.util.List;
+
 public class ContatoAdapter extends BaseAdapter{
+
+    private List<Contato> contatos;
+    Activity act;
+
+    public ContatoAdapter(List<Contato> contatos, Activity act){
+        this.contatos = contatos;
+        this.act = act;
+    }
+
+
+
     @Override
     public int getCount() {
-        return 0;
+        return this.contatos.size();
     }
 
     @Override
