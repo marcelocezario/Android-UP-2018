f7afdd63209474f25cd4695d4f6d0097f133ada1
Marcelo Cezário <marcelocezario@gmail.com>
2018-06-11 19:37:52 -0300

    Criando construtor

--- Contatos/app/src/main/java/up/edu/br/contatos/Conexao.java
+++ Contatos/app/src/main/java/up/edu/br/contatos/Conexao.java
@@ -1,14 +1,19 @@
 package up.edu.br.contatos;
 
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class Conexao extends SQLiteOpenHelper{
 
 
+    public Conexao(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
     @Override
     public void onCreate(SQLiteDatabase db) {
-        
+
     }
 
     @Override
