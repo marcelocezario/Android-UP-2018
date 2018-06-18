c91662a815356fc083df6b3eb3edb7e24b3fc142
Marcelo Cezário <marcelocezario@gmail.com>
2018-06-18 19:22:21 -0300

    Criando novo projeto Navegador

---
+++ Navegador/app/src/main/java/up/edu/br/navegador/MainActivity.java
@@ -0,0 +1,13 @@
+package up.edu.br.navegador;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
