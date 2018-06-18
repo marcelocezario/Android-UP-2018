91c2aaf12991a912bf47bd0c2027fa50bf0e89ad
Marcelo Cezário <marcelocezario@gmail.com>
2018-06-18 19:30:31 -0300

    Criando tela e método para abrir endereço no navegador

--- Navegador/app/src/main/java/up/edu/br/navegador/MainActivity.java
+++ Navegador/app/src/main/java/up/edu/br/navegador/MainActivity.java
@@ -2,6 +2,9 @@ package up.edu.br.navegador;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void carregar(View v){
+        TextView textView = (TextView)findViewById(R.id.editText);
+
+        WebView navegador = (WebView)findViewById(R.id.navegador);
+
+        navegador.getSettings().setJavaScriptEnabled(true);
+        navegador.loadUrl(textView.getText().toString());
     }
 }
