72f74e46c2ff00a2a94309c28a1a006d511d080f
Marcelo Cezário <marcelocezario@gmail.com>
2018-06-20 19:41:35 -0300

    Criando método para adicionar imagem da camera no contato

--- Contatos/app/src/main/java/up/edu/br/contatos/ContatoActivity.java
+++ Contatos/app/src/main/java/up/edu/br/contatos/ContatoActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,6 +14,7 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -55,6 +57,16 @@ public class ContatoActivity extends AppCompatActivity {
 
 
         }
+
+        ImageView image = (ImageView) findViewById(R.id.image);
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            }
+        });
+
     }
 
     @Override
